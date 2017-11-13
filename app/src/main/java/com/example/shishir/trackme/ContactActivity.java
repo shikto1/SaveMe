05f020dd759e2c124f3e1f2c3f50a2f35001faf5
shikto1 <islamahidul1@gmail.com>
2017-11-14 00:19:39 +0600

    //Shishir

--- app/src/main/java/com/example/shishir/trackme/ContactActivity.java
+++ app/src/main/java/com/example/shishir/trackme/ContactActivity.java
@@ -1,7 +1,9 @@
 package com.example.shishir.trackme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,6 +11,18 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String from = intent.getStringExtra("from");
+            if (from.equals("contact")) {
+                ((TextView)findViewById(R.id.test)).setText("Contact");
+            } else if (from.equals("interval")) {
+                ((TextView)findViewById(R.id.test)).setText("Interval");
+            } else if (from.equals("sms")) {
+                ((TextView)findViewById(R.id.test)).setText("SMS");
+            }
+        }
     }
 
     @Override
