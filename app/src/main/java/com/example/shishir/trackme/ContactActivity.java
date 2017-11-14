a10bdbc2b37adf75fd097e6e8e984c53664921a5
shikto1 <islamahidul1@gmail.com>
2017-11-14 22:05:25 +0600

    //Shishir

--- app/src/main/java/com/example/shishir/trackme/ContactActivity.java
+++ app/src/main/java/com/example/shishir/trackme/ContactActivity.java
@@ -3,26 +3,37 @@ package com.example.shishir.trackme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
+
+    LinearLayout contactL, intervalL,messageL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
+        findViewById();
         Intent intent = getIntent();
-        if (intent != null) {
-            String from = intent.getStringExtra("from");
-            if (from.equals("contact")) {
-                ((TextView)findViewById(R.id.test)).setText("Contact");
-            } else if (from.equals("interval")) {
-                ((TextView)findViewById(R.id.test)).setText("Interval");
-            } else if (from.equals("sms")) {
-                ((TextView)findViewById(R.id.test)).setText("SMS");
-            }
-        }
+//        if (intent != null) {
+//            String from = intent.getStringExtra("from");
+//            if (from.equals("contact")) {
+//                contactL.setVisibility(View.VISIBLE);
+//            } else if (from.equals("interval")) {
+//                intervalL.setVisibility(View.VISIBLE);
+//            } else if (from.equals("sms")) {
+//                messageL.setVisibility(View.VISIBLE);
+//            }
+//        }
+    }
+
+    private void findViewById() {
+        contactL=(LinearLayout)findViewById(R.id.contactLayout);
+        intervalL=(LinearLayout)findViewById(R.id.intervalLayout);
+        messageL=(LinearLayout)findViewById(R.id.messageFormatLayout);
     }
 
     @Override
